7a80f0deeabba061f89d1195faaeb4b748b54b8d
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-02-25 17:57:58 +0530

    next

---
+++ Intro/LiteralsConstants.py
@@ -0,0 +1,49 @@
+#In Python, 
+# literals are notations used to represent fixed values in the 
+# source code, and constants are variables whose values should not be modified during program execution.
+# Let's explore these concepts in more detail:
+
+#Numeric Types:
+
+# Literals:
+
+# Numeric Literals:
+
+# Integer literals: Examples include 42, 0, -7.
+# Floating-point literals: Examples include 3.14, 2.0, -0.5.
+# Complex literals: Examples include 1 + 2j, -3.5 - 4j.
+# String Literals:
+
+# Single-line strings: Enclosed in single (') or double (") quotes.
+
+single_line_mssg = 'Hello, World!'
+
+multiline_message = '''
+This is a
+multi-line
+string.
+'''
+
+my_list = [1, 2, 3]
+my_tuple = (4, 5, 6)
+my_set = {7, 8, 9}
+my_dict = {'a': 10, 'b': 11, 'c': 12}
+
+# Constants:
+
+# ->In Python, there is no strict concept of constants 
+# as in some other programming languages.
+
+# ->Conventionally, variables whose values are not intended 
+# to be changed are sometimes referred to as constants.
+
+# ->By convention, names of constants are often written in uppercase to indicate that they are intended not to be modified, but this is not enforced by the language.
+# Example of using uppercase for constants by convention:
+
+
+PI = 3.14159
+GRAVITY = 9.8
+# It's important to note that Python does not enforce the immutability of variables declared as constants. The convention is followed to indicate the programmer's intention that the value should not be modified.
+
+# In summary, literals are fixed values represented directly in code, while constants, in the context of Python, are often represented by variables whose values are not intended to be changed, following a naming convention.
+
