9264cf0a769e82b4f0bdee1c65d9bcc10de0bf12
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-02-25 17:45:57 +0530

    dataTypes

---
+++ DataTypes/dataTypesIntro.py
@@ -0,0 +1,38 @@
+# numeric data types
+# int , float , complex
+a = 5       # int
+b = 3.14    # float
+c = 2 + 3j  # complex
+
+
+#sequece Types
+# str -> string
+#list: list -> mutable , ordered sequence
+# tuple : immutable , ordered
+text = "Hello, Python!"      # str
+my_list = [1, 2, 3, 'a']     # list
+my_tuple = (4, 5, 6, 'b')    # tuple
+
+#set Types
+#set : unordered collecvtion of unique elemnts
+#frozenset: immutable set
+st={1,2,3,3,2}
+frozem_set=([1,2,3,4])
+
+#dict
+my_dict = {'name': 'John', 'age': 25}  # dict
+
+
+#boolean type
+is_true = True   # bool
+is_false = False  # bool
+
+
+#none type
+my_var = None  # NoneType
+
+
+#binary types
+binary_data = b'hello'        # bytes
+mutable_binary = bytearray(b'world')  # bytearray
+view = memoryview(b'example')  # memoryview
